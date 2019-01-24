b890b33ab20ad21135569e0175a182388964a094
Benoit Meylan <Benoit.Meylan@cpnv.ch>
2019-01-24 09:06:48 +0100

    résolution des conflits

---
+++ ConstructionSiteLib/ConstructionSite.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructionSiteLib
+{
+    class ConstructionSite
+    {
+        private string name;
+        private List<Machine> machines = new List<Machine>();
+        private List<Employee> employees = new List<Employee>();
+        private int capital;
+
+
+        public ConstructionSite(string name, int capital)
+        {
+            this.name = name;
+            this.capital = capital; 
+        }
+
+        public void BuyMachine(Machine machine)
+        {
+
+        }
+
+        public void HireEmployee(Employee employee)
+        {
+
+        }
+    }
+}
---
+++ ConstructionSiteLib/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructionSiteLib
+{
+    class Employee
+    {
+        private string name;
+        private int age;
+        private int salary;
+
+        public Employee(string name, int age, int salary)
+        {
+            this.name = name;
+            this.age = age;
+            this.salary = salary;
+        }
+
+        public void Work(int hours)
+        {
+
+        }
+    }
+}
