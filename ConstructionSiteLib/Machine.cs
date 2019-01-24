06f245162e1552a8cbe49e840f8d4b118b4cc326
Benoit Meylan <Benoit.Meylan@cpnv.ch>
2019-01-24 08:46:48 +0100

    Ajout des classes

---
+++ ConstructionSiteLib/Machine.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructionSiteLib
+{
+    class Machine
+    {
+        private Employee employee;
+        private int price = 0;
+        private int productivity = 0;
+        private bool operational = true;
+        private string brand = "";
+        private int weight = 0; //weight in Kg
+
+        public Machine(int price, int productivity, string brand, int weight)
+        {
+            this.employee = employee;
+            this.price = price;
+            this.productivity = productivity;
+            this.brand = brand;
+            this.weight = weight;
+        }
+
+        public void Assign(List<Employee> employees)
+        {
+            foreach(Employee employee in employees)
+            {
+                //TO DO
+            }
+        }
+
+        public int Repair()
+        {
+
+        }
+    }
+}
